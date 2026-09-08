9cf495e891958a12dcdb42bd973681bed1be3934
Augustine21-ui <augustinekinyua32@gmail.com>
2026-09-08 10:18:31 +0300

    Increase holographic avatar size

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -597,7 +597,7 @@ export default function DashboardPage() {
             <div className="flex-shrink-0 cursor-pointer" onClick={() => setShowMoodModal(true)}>
               <HolographicAvatar 
                 mood={autoMood} 
-                size={80} 
+                size={129} 
                 animation="float"
                 imageMap={avatarImageMap}   // ← added image mapping
                 onClick={() => setShowMoodModal(true)} 
