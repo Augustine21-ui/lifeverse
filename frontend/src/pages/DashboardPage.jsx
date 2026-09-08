9db382c95d1f0d238566c1e2275b230002110170
Augustine21-ui <augustinekinyua32@gmail.com>
2026-09-08 12:36:38 +0300

    updated dashbaord page

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -601,7 +601,7 @@ export default function DashboardPage() {
             <div className="flex-shrink-0 cursor-pointer" onClick={() => setShowMoodModal(true)}>
               <HolographicAvatar 
                 mood={autoMood} 
-                size={100} 
+                size={80} 
                 animation="float"
                 imageMap={avatarImageMap}   // ← added image mapping
                 onClick={() => setShowMoodModal(true)} 
